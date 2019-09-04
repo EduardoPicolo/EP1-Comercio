735239fcfc0ef9e74d4023d2c6e6a1be9260c291
EduardoPicolo <eduardonpicolo@gmail.com>
2019-09-04 16:54:04 -0300

    Mudança na declaração/implementação das funções com template

--- inc/Functions.hpp
+++ inc/Functions.hpp
@@ -12,41 +12,19 @@
 #include <vector>
 #include <string>
 #include <limits>
-
 using namespace std;
 
 int start(void);
 void fill_string_spaces(string & str);
 
-template<typename myClass>
-void over_write(string file_name, vector<myClass> list){
-    fstream file(file_name, ios::out);
-    for(size_t i=0; i<list.size(); i++){
-        file<<list[i];
-    }
-    file.close();  
-}
+template<class myClass>
+void over_write(string file_name, vector<myClass> list);
 
 template<typename myClass>
-vector<myClass> read_file(string file_name){
-    vector<myClass> objList;
-    fstream file;
-    file.open(file_name, ios::in);
-    myClass temp;
-    while(file>>temp){
-        objList.emplace_back(temp);
-    }
-    file.close();
-    return objList;
-}
+vector<myClass> read_file(string file_name);
 
 template<typename myClass>
-void write_file(string file_name, myClass obj){
-    // Write new obj to file
-    fstream file;
-    file.open(file_name, ios::app);
-    file<< obj;
-    file.close();
-}
+void write_file(string file_name, myClass obj);
 
+#include "Functions.tpp"
 #endif
